26a266bd369f59c96da74f285507b3231e669bf5
herman9x9 <hoob1234561@gmail.com>
2025-01-14 16:33:52 +0800

    Update index.js

--- week2/index.js
+++ week2/index.js
@@ -3,37 +3,37 @@
 // 若不確定答案，可將 code 貼在 chrome console 顯示後，再回頭補知識點。
 let a = 9;
 let b = 13;
-console.log(a > 0);
-console.log(b > a);
-console.log(a + b > 1);
+console.log(a > 0);//true
+console.log(b > a);//true
+console.log(a + b > 1);//true
 let c = 51;
 let d = 163;
-console.log(c == d);
-console.log(c !== d);
+console.log(c == d);//false
+console.log(c !== d);//true
 let e = 28;
 let f = 45;
-console.log(f >= e);
-console.log(f != e);
-console.log(f == e);
+console.log(f >= e);//true
+console.log(f != e);//true
+console.log(f == e);//false
 
 // ### 題目二：比較運算子 + 強制轉型
 // 請回答每個 console.log 的值為？
 let g = 8;
 let h = '8';
-console.log(g * h == 88);
-console.log(g * h == 64);
-console.log(g * h === 64);
+console.log(g * h == 88);//false
+console.log(g * h == 64);//true
+console.log(g * h === 64);//true
 
 let i = '9';
 let j = '9';
-console.log(i + j == 99);
-console.log(i + j === '99');
-console.log(i + j === 99);
+console.log(i + j == 99);//true
+console.log(i + j === '99');//true
+console.log(i + j === 99);//false
 
 var k = 3;
 var l = '8';
 // 請文字解釋為什麼
-console.log(k * l > 21);
+console.log(k * l > 21);//js會自動把string'8'轉為數字8，所以3*8>21是true
 
 // ### 題目三：邏輯運算子 + if, else
 // 情境：健身房週年慶，買課程送贈品
@@ -47,15 +47,13 @@ let giftNum = 280; /* 贈品數量 */
 let giftPriceRule = 1599; /* 贈品消費門檻 */
 let BobPrice = 1800; /* Bob 消費金額 */
 let BobIsVip = false; /* Bob 是否為 VIP */
-
-// 練習：（可自行將下方程式碼的註解刪除，完成答題）
-// if () {
-//   console.Log（"客戶您好，您有符合贈品資格"）；
-// } else {
-//   console.Log（"客戶您好，您沒有符合贈品資格"）
-// }
-
-console.log(`贈品還剩下${giftNum}個`);
+if (BobPrice>=1599 || BovIsVip===true){//判斷金額達到門檻或是否為VIP
+  console.log("客戶您好，您有符合贈品資格");
+  giftNum --;}//符合贈品資格，所以數量-1
+else{
+  console.log("客戶您好，您沒有符合贈品資格");
+}
+console.log(`贈品還剩下${giftNum}個`);//Bob有符合，所以現在贈品數量=279
 
 // ### 題目四：
 // 健身房的業績獎金計算
@@ -67,13 +65,23 @@ console.log(`贈品還剩下${giftNum}個`);
 
 // 例如小華的全年業績為 8 萬：80,000 * 0.1 + 6,000 總計需要支付 14,000 元獎金。
 
+
+// 練習：計算教練業績獎金
 let coachIncome = 240000; // 小明全年業績
 let baseBonus = 6000; // 基本獎金
 let coachBonus = baseBonus; // 教練業績獎金帳單，並已加入條件一基本獎金
 
-// 練習：計算教練業績獎金
+// 計算教練業績獎金
+if (coachIncome > 300000) {
+  coachBonus += coachIncome * 0.2; // 超過 30 萬，20% 獎金
+} else if (coachIncome > 100000 && coachIncome <= 300000) {
+  coachBonus += coachIncome * 0.15; // 10 萬 ~ 30 萬，15% 獎金
+} else if (coachIncome <= 100000) {
+  coachBonus += coachIncome * 0.1; // 10 萬以下，10% 獎金
+}
 
 console.log(`小明總共需支付 $${coachBonus} 獎金`);
+//小明總共需支付42000獎金
 
 // ### 題目五：剪刀石頭布
 // 請寫程式來判斷剪刀石頭布的輸贏
